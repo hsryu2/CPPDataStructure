5e1dc4fc504a1fcaa1a52eda0d0161b7ac09b3c0
hsryu2 <thryu9376@gmail.com>
2026-02-23 09:59:44 +0900

    이진 탐색 트리 구현 시작
    - 노드 클래스 추가
    - 이진 탐색 트리 클래스 추가

--- 11.BST/Container/Node.h
+++ 11.BST/Container/Node.h
@@ -1,1 +1,32 @@
 #pragma once
+
+
+#include <iostream>
+
+template<typename T>
+
+class Node
+{
+	template<typename T>
+	friend class BinarySearchTree;
+public:
+	Node(const T& data, const Node<T>* const parent = nullptr)
+		: data(data), parent(parent)
+	{
+	}
+
+private:
+	// 데이터 변수
+	T data = T();
+
+	// 부모 노드
+	Node<T>* parent = nullptr;
+
+	// 왼쪽 자식 노드
+	Node<T>* left = nullptr;
+
+	// 오른쪽 자식 노드.
+	Node<T>* right = nullptr;
+
+
+};
