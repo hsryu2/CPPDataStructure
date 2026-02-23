50e82d5f17e6fb71de9701a4025aed94543cc61d
hsryu2 <thryu9376@gmail.com>
2026-02-23 11:02:50 +0900

    삽입/검색 함수 구현/삭제 함수 구현 중
    - 삽입/검색 함수 구현 (재귀함수 포함)
    - 삭제 재귀 함수 구현 중 - 부분 검색함수 구현할 차례

--- 11.BST/Container/Node.h
+++ 11.BST/Container/Node.h
@@ -1,32 +1,32 @@
 #pragma once
 
-
 #include <iostream>
 
+// 이진 트리에 사용할 노드 클래스.
 template<typename T>
-
 class Node
 {
+	// friend 선언.
 	template<typename T>
 	friend class BinarySearchTree;
+
 public:
-	Node(const T& data, const Node<T>* const parent = nullptr)
+	Node(const T& data,
+		Node<T>* const parent = nullptr)
 		: data(data), parent(parent)
 	{
 	}
 
 private:
-	// 데이터 변수
+	// 데이터 변수.
 	T data = T();
 
-	// 부모 노드
+	// 부모 노드.
 	Node<T>* parent = nullptr;
 
-	// 왼쪽 자식 노드
+	// 왼쪽 자손 노드.
 	Node<T>* left = nullptr;
 
-	// 오른쪽 자식 노드.
+	// 오른쪽 자손 노드.
 	Node<T>* right = nullptr;
-
-
 };
--- 11.BST/main.cpp
+++ 11.BST/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "Container/BinarySearchTree.h"
 
 int main()
 {
+	// 정수형 이진 탐색 트리 객체 생성.
+	BinarySearchTree<int> tree;
+
+	// 삽입.
+	tree.InsertNode(10);
+	tree.InsertNode(20);
+	tree.InsertNode(5);
+	tree.InsertNode(30);
+
+	std::cin.get();
 
 }
